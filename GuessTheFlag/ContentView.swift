e96039d74868f94519c542ca8a250ca6ace55f33
Anwar A. Ruff <anwarruff@gmail.com>
2022-11-04 18:11:02 -0400

    Add random selection of three flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var randomStartPoint : Int
+    var randomEndPoint : Int
+    var randomFlag : Int
+    
+    
+    init(){
+        randomStartPoint = Int.random(in: 0...8)
+        randomEndPoint = randomStartPoint + 2
+        randomFlag = Int.random(in: randomStartPoint...randomEndPoint)
+    }
     
     var body: some View {
         VStack(spacing: 30) {
-            UITextView("Instructions")
-            ForEach(0..<3, id: \.self) { i in
+            VStack {
+                Text("Pick the flag for \(countries[randomFlag])")
+            }
+            ForEach(randomStartPoint...randomEndPoint, id: \.self) { i in
                 Button(action: {print("clicked on \(countries[i])")}, label: {
                     Image("\(countries[i])")
                         .renderingMode(.original)
@@ -21,7 +33,6 @@ struct ContentView: View {
                 })
             }
         }
-        .padding()
     }
 }
 
