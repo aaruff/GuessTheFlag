c97bc47ab28fe0fe96e089bab7011a7870870eac
Anwar A. Ruff <anwarruff@gmail.com>
2022-11-06 11:08:41 -0500

    Refactored method for selecting random flags for new games

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,29 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var randomStartPoint : Int
-    var randomEndPoint : Int
-    var randomFlag : Int
+    let flagsPerChoice = 3
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    var randomChosenFlag : Int
+    
+    @State var score = 0
+    @State var choiceMade = false
+    @State var choiceOutcome = ""
+    
+    func generateNewQuestion() {
+        countries.shuffled()
+        choiceMade = false
+    }
+    
+    func flagSelectionHandler(choice: Int) {
+        if choice == randomChosenFlag {
+            choiceOutcome = "Correct"
+        }
+        else {
+            choiceOutcome = "Incorrect"
+        }
+            
+        choiceMade = true
+    }
     
     
     init(){
-        randomStartPoint = Int.random(in: 0...8)
-        randomEndPoint = randomStartPoint + 2
-        randomFlag = Int.random(in: randomStartPoint...randomEndPoint)
+        randomChosenFlag = Int.random(in: 0..<3)
     }
     
     var body: some View {
-        VStack(spacing: 30) {
-            VStack {
-                Text("Pick the flag for \(countries[randomFlag])")
-            }
-            ForEach(randomStartPoint...randomEndPoint, id: \.self) { i in
-                Button(action: {print("clicked on \(countries[i])")}, label: {
-                    Image("\(countries[i])")
-                        .renderingMode(.original)
-                        .border(.black)
-                })
+        NavigationView {
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Pick the flag for \(countries[randomChosenFlag])")
+                }
+                ForEach(0..<flagsPerChoice, id: \.self) { i in
+                    Button(
+                        action: {
+                            flagSelectionHandler(choice: i)
+                        },
+                        label: {
+                        Image("\(countries[i])")
+                            .renderingMode(.original)
+                            .border(.black)
+                        }
+                    )
+                    .alert(choiceOutcome, isPresented: $choiceMade) {
+                        Button("OK", role: .cancel){
+                            choiceMade = false
+                        }
+                    }
+                }
+                .navigationBarTitle("Guess The Flag")
             }
         }
     }
